d6766014f19ebe9ecda3a12fd813c14829d85a0b
Mohak Kant <mohak.kant1amg@gmail.com>
2019-10-24 12:26:18 -0500

    Debugging Transfer

--- evolutionary_algorithms/experimenthost/glo/evolution/session_server.py
+++ evolutionary_algorithms/experimenthost/glo/evolution/session_server.py
@@ -73,6 +73,7 @@ class SessionServer:
             population.generate_mating_pool()
             for child_num in range(self.population_size):
                 parents = population.natural_selection()
+                print(parents)
                 parents = TreeUtils.sort_trees_by_fitness_desc(parents)
                 child = Crossover.crossover(parents[0], parents[1])
                 child = Mutation.weighted_function_mutation(child, self.weighted_function_mutation_rate)
