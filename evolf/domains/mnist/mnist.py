b5e1302e8b9118cb951c9d36d28cc72554c71e33
mohakamg <mohak.kant1amg@gmail.com>
2019-11-04 23:05:03 -0600

    Update mnist.py

--- evolf/domains/mnist/mnist.py
+++ evolf/domains/mnist/mnist.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import argparse
-from evolf.domains.mnist.generate_mnist_data import GenerateMnistData
-from evolf.domains.mnist.network_constructor import NetworkConstructor
-=======
 
 from evolf.domains.mnist.get_mnist_data import GetMnistData
 from evolf.domains.mnist.mnist_network_constructor import MnistNetworkConstructor
->>>>>>> 2b4ac31f582bd602a247916d08053d4e924117e0
 from evolf.evolution.session_server import SessionServer
 
 if __name__ == "__main__":
